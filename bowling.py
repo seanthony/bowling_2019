164cfe05a53a23b7f9488840bd2554a0420ac132
Sean Anthony <sean@basecampcodingacademy.org>
2019-08-12 09:38:26 -0500

    fixed an error in updating the frames list
    silly mistake of not properly copied code

--- bowling.py
+++ bowling.py
@@ -6,7 +6,7 @@ def play_game():
         first_attempt = int(input('  >>> first throw? '))
         if first_attempt < 10:
             second_attempt = int(input('  >>> second throw? '))
-            frames[i] = [first_attempt, second_attempt]
+            frames.append([first_attempt, second_attempt])
         else:
             frames.append([first_attempt])
 
